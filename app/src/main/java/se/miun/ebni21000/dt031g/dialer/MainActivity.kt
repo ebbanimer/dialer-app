4ded6d9cc06e00315a2625e2038ed6d320a16ca4
ebbanimer <ebni2100@student.miun.se>
2022-11-01 20:05:10 +0100

    Added About dialog

--- app/src/main/java/se/miun/ebni21000/dt031g/dialer/MainActivity.kt
+++ app/src/main/java/se/miun/ebni21000/dt031g/dialer/MainActivity.kt
@@ -1,8 +1,11 @@
 package se.miun.ebni21000.dt031g.dialer
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import se.miun.ebni21000.dt031g.dialer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -42,11 +45,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-//        binding.dialBtn.setOnClickListener {
-//            val intent = Intent(this, DialActivity::class.java)
-//            startActivity(intent)
-//        }
+        binding.aboutBtn.setOnClickListener {
+            aboutDialog()
+        }
+    }
 
+    // Show the about dialog
+    private fun aboutDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder///.setTitle()
+            .setMessage("This app is supposed to mimic the keypad  \n" +
+                "on a phone. The app will consist of\n" +
+                "activities to:\n" +
+                "\n" +
+                "- Enter numbers to dial\n" +
+                "- See previously dialed numbers\n" +
+                "- Change the keypad settings\n" +
+                "- Show on a Map where previous calls are \n" +
+                "dialed from \n")
+            .setPositiveButton("Ok") { dialog, _ -> dialog.cancel()
+            }
 
+        val alert = builder.create()
+        // set title for alert dialog box
+        alert.setTitle("About")
+        // show alert dialog
+        alert.show()
+
+        //builder.show()
     }
 }
