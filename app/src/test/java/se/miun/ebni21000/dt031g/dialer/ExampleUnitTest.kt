30f8b40281fc38a0b19caae64d4c390e5c2d4128
ebbanimer <ebni2100@student.miun.se>
2022-11-01 14:55:01 +0100

    Android project created.

---
+++ app/src/test/java/se/miun/ebni21000/dt031g/dialer/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package se.miun.ebni21000.dt031g.dialer
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
