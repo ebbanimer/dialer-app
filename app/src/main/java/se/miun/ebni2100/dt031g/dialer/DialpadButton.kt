babb77ae11c55c5e44a1cfc22c48bf692404cf7e
ebbanimer <ebni2100@student.miun.se>
2022-11-03 19:21:58 +0100

    Task 2: DialpadButton class with layout added.

---
+++ app/src/main/java/se/miun/ebni2100/dt031g/dialer/DialpadButton.kt
@@ -0,0 +1,30 @@
+package se.miun.ebni2100.dt031g.dialer
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import se.miun.ebni2100.dt031g.dialer.databinding.DialpadlayoutBinding
+
+class DialpadButton @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attributeSet, defStyle) {
+
+    private var binding : DialpadlayoutBinding
+
+    init {
+        binding = DialpadlayoutBinding.bind(
+            View.inflate(context, R.layout.dialpadlayout,this)
+        )
+    }
+
+    fun setTitle(title: String){
+        binding.btnTitle.text = title
+    }
+
+    fun setMessage(message: String){
+        binding.btnMessage.text = message
+    }
+}
