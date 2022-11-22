f4e4f45014134e2e609e5c9b659aaa223dfeffa8
ebbanimer <ebni2100@student.miun.se>
2022-11-22 22:17:15 +0100

    Task5: Subtask 2, added buttons in main

---
+++ app/src/main/java/se/miun/ebni2100/dt031g/dialer/DownloadActivity.kt
@@ -0,0 +1,11 @@
+package se.miun.ebni2100.dt031g.dialer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DownloadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_download)
+    }
+}
