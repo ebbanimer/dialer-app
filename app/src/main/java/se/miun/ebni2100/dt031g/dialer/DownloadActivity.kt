5aab22d86cc9975f5c7a651952cdc6d83a46fb0c
ebbanimer <ebni2100@student.miun.se>
2022-11-22 22:53:34 +0100

    Task5: Subtask 2, added webview

--- app/src/main/java/se/miun/ebni2100/dt031g/dialer/DownloadActivity.kt
+++ app/src/main/java/se/miun/ebni2100/dt031g/dialer/DownloadActivity.kt
@@ -1,11 +1,35 @@
 package se.miun.ebni2100.dt031g.dialer
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import se.miun.ebni2100.dt031g.dialer.databinding.ActivityDialBinding
+import se.miun.ebni2100.dt031g.dialer.databinding.ActivityDownloadBinding
 
 class DownloadActivity : AppCompatActivity() {
+
+    var binding : ActivityDownloadBinding? = null
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download)
+
+        binding = ActivityDownloadBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.webView?.webViewClient  = WebViewClient()
+        binding?.webView?.loadUrl("https://dt031g.programvaruteknik.nu/dialer/voices/")
+
+        // enable the javascript settings
+        binding?.webView?.settings?.javaScriptEnabled  = true
+
     }
+
 }
